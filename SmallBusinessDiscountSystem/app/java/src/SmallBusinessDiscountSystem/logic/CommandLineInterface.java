850a0ac54a0b76490c5163f0456eacc3eb60ddee
Maria João Mira Paulo <mariajoaomirapaulo1996@gmail.com>
2017-12-27 19:51:31 +0000

    Starting gui

--- SmallBusinessDiscountSystem/app/java/src/SmallBusinessDiscountSystem/logic/CommandLineInterface.java
+++ SmallBusinessDiscountSystem/app/java/src/SmallBusinessDiscountSystem/logic/CommandLineInterface.java
@@ -1,21 +1,27 @@
 package SmallBusinessDiscountSystem.logic;
 
+import SmallBusinessDiscountSystem.gui.Customer;
 import SmallBusinessDiscountSystem.gui.DiscountSystem;
 import SmallBusinessDiscountSystem.gui.Merchant;
 
+import java.util.Scanner;
+
 
 public class CommandLineInterface {
+
+	DiscountSystem system = new DiscountSystem();
+	Scanner scanner = new Scanner(System.in);
 
 	public CommandLineInterface(){}
 	
 	public static void main(String[] args){
 		CommandLineInterface cli = new CommandLineInterface();
 
-		launchMainMenu();
+		cli.launchMainMenu();
 
 	}
 
-	public static void launchMainMenu(){
+	public void launchMainMenu(){
 
 		System.out.println("---------------------------------------------");
 		System.out.println("            Welcome to Boobonus!");
@@ -24,15 +30,19 @@ public class CommandLineInterface {
 		System.out.println("1. Merchant");
 		System.out.println("2. Costumer");
 		System.out.println("3. Exit");
+		System.out.println("");
 
-		int option = Integer.parseInt(System.console().readLine());
+
+		int option = scanner.nextInt();
+		// Skip the newline
+		scanner.nextLine();
 
 		switch (option){
 			case 1:
 				launchMerchantMenu();
 				break;
 			case 2:
-				//launchCustomerMenu();
+				launchCustomerMenu();
 				break;
 			case 3:
 				System.exit(1);
@@ -44,16 +54,34 @@ public class CommandLineInterface {
 
 	}
 
-	public static void launchMerchantMenu(){
-
-		DiscountSystem system = new DiscountSystem();
+	public void launchMerchantMenu(){
 
 		System.out.println("What is the name of your company?");
-		String name = System.console().readLine();
 
+		String name = scanner.nextLine();
 		Merchant merchant = new Merchant(name);
+		system.merchantJoins(merchant);
 
+		System.out.println("Successfully added to system. Press Enter to go back!");
 
+		scanner.nextLine();
+
+		launchMainMenu();
+	}
+
+	public void launchCustomerMenu(){
+
+		System.out.println("What is your name?");
+
+		String name = scanner.nextLine();
+		Customer customer = new Customer(name);
+		system.customerJoins(customer);
+
+		System.out.println("Successfully added to system. Press Enter to go back!");
+
+		scanner.nextLine();
+
+		launchMainMenu();
 	}
 
 }
