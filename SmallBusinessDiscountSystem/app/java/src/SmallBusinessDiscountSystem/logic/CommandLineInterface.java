cbabc8d77e953ae424e8f27966a8e73f77ec4ef7
Pedro Costa <pedrodc23@gmail.com>
2017-12-27 18:47:09 +0000

    Started interface

--- SmallBusinessDiscountSystem/app/java/src/SmallBusinessDiscountSystem/logic/CommandLineInterface.java
+++ SmallBusinessDiscountSystem/app/java/src/SmallBusinessDiscountSystem/logic/CommandLineInterface.java
@@ -1,7 +1,8 @@
 package SmallBusinessDiscountSystem.logic;
 
-import java.io.IOException;
-import java.util.*;
+import SmallBusinessDiscountSystem.gui.DiscountSystem;
+import SmallBusinessDiscountSystem.gui.Merchant;
+
 
 public class CommandLineInterface {
 
@@ -9,7 +10,50 @@ public class CommandLineInterface {
 	
 	public static void main(String[] args){
 		CommandLineInterface cli = new CommandLineInterface();
-		System.out.println("oi");
+
+		launchMainMenu();
+
+	}
+
+	public static void launchMainMenu(){
+
+		System.out.println("---------------------------------------------");
+		System.out.println("            Welcome to Boobonus!");
+		System.out.println("---------------------------------------------");
+		System.out.println("What are you?");
+		System.out.println("1. Merchant");
+		System.out.println("2. Costumer");
+		System.out.println("3. Exit");
+
+		int option = Integer.parseInt(System.console().readLine());
+
+		switch (option){
+			case 1:
+				launchMerchantMenu();
+				break;
+			case 2:
+				//launchCustomerMenu();
+				break;
+			case 3:
+				System.exit(1);
+				break;
+			default:
+				launchMainMenu();
+				break;
+		}
+
+	}
+
+	public static void launchMerchantMenu(){
+
+		DiscountSystem system = new DiscountSystem();
+
+		System.out.println("What is the name of your company?");
+		String name = System.console().readLine();
+
+		Merchant merchant = new Merchant(name);
+
+
 	}
 
 }
